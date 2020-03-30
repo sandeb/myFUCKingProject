94c278e8f4c85a972ef09ae82c8c5fb1fdf11de8
sandeb <sandrine.debatty@gmail.com>
2020-03-31 01:49:38 +0200

    a fucking unsave

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
         <div class="card"><img
                 src="https://images.unsplash.com/photo-1425342605259-25d80e320565?auto=format&amp;fit=crop&amp;w=750&amp;q=80&amp;ixid=dW5zcGxhc2guY29tOzs7Ozs%3D" />
             <div class="info">
-                <h1>Road</h1>
                 <p>Lorem Ipsum is simply dummy text from the printing and typeseting industry</p>
                 <button>Read More</button>
             </div>
